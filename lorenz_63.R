fd215b80e2dc822b320bf68095c560dc2fc0998c
kushagragpt99 <glaedur@inheritance.localdomain>
2020-09-15 19:47:12 +0530

    adding comments

--- lorenz_63.R
+++ lorenz_63.R
@@ -3,25 +3,33 @@ library(mvtnorm)
 library(matrixcalc)
 library(mcmc)
 
+# drifet function for Lorenz-63
 drift_fun <- function(X, theta) {
     ans = c(theta[1] * (X[2] - X[1]), theta[2] * X[1] - X[2] - X[1] * X[3], X[1] * X[2] - theta[3] * X[3])
     return(t(t(ans)))
 }
 
-ludfun <- function(state) {
-    
+# logarithm of the unnormalized posterior
+ludfun <- function(state) {     
+    # State is the vector storing the vectors of length 3*N + 12. The first 3*(N+1) terms are Xs. The next three terms are the parameters \sigma, \rho & 
+    # \beta. The remaining 6 terms are the \Sigma matrix. Definition of Sigma below shows how the symmetric matrix is constructed.
+
     X_n = matrix(state[1:(3 * (N + 1))], nrow = 3, ncol = N + 1)
     Sigma_vec = state[(3 * N + 7):(3 * N + 12)]
     Sigma = matrix(c(Sigma_vec[1], Sigma_vec[2], Sigma_vec[3], Sigma_vec[2], Sigma_vec[4], Sigma_vec[5], Sigma_vec[3], Sigma_vec[5], Sigma_vec[6]), nrow = 3)
-    theta = state[(3 * N + 4):(3 * N + 6)]
+    theta = state[(3 * N + 4):(3 * N + 6)]  # vector of \sigma, \rho and \beta
+
+    # all the elements of theta should be positive
     if (min(theta) <= 0)
         return(-Inf)
+    # \Sigma should be positive semi-definite
     if (is.positive.semi.definite(Sigma) == FALSE)
         return(-Inf)
 
     X_t = X_n[, seq(1, N + 1, N / K)]
     inv_R = solve(R)
 
+    # pi is the log of likelihood
     p1 = 0
     for (k in 1:K) {
         Y.t = t(t(Y[, k]))
@@ -30,6 +38,7 @@ ludfun <- function(state) {
     }
     p1 = -0.5 * p1
 
+    # p2 is the log of prior of X conditional on theta
     p2 = 0
     
     inv_Sig = solve(Sigma)
@@ -43,14 +52,14 @@ ludfun <- function(state) {
     p2 = -0.5 * p2
     p2 = p2 - 0.5 * t(t(t(X_n[,1])) - tau_o) %*% solve(lam_o) %*% (t(t(X_n[,1])) - tau_o) - (N / 2) * log(det(Sigma * del_t))
 
-    
+    # p3 is the lof og priors of theta
     p3 = (alpha1 - 1) * log(theta[1]) - theta[1] / beta1 + (alpha2 - 1) * log(theta[2]) - theta[2] / beta2 + (alpha3 - 1) * log(theta[3]) - theta[3] / beta3
 
     return(p1 + p2 + p3)
 
 }
 
-
+# Numerical method to sample from SDE
 euler_maruyama <- function(X0, del_t, N, theta, Sigma) {
     X = matrix(, nrow = 3, ncol = N + 1)
     X[, 1] = X0
@@ -60,29 +69,30 @@ euler_maruyama <- function(X0, del_t, N, theta, Sigma) {
 }
 # X = euler_maruyama(c(1,1,1), 0.1, 20, c(1,2,3), diag(2,3))
 
-to = 0
-tf = 20
-Nobs = 10
-del_t = 0.01
-tau_o = matrix(rep(0, 3), nrow = 3, ncol = 1)
-lam_o = diag(1, 3)
-alpha1 = 20
-alpha2 = 56
-alpha3 = 6
+# hyper-parameters
+to = 0                                                  # initial time
+tf = 20                                                 # final time
+Nobs = 10                                               # no of observations (Y) per time step
+del_t = 0.01                                            # discrete approximation of dt
+tau_o = matrix(rep(0, 3), nrow = 3, ncol = 1)           # prior mean for X[0], i.e. initial state of Lorenz-63 oricess
+lam_o = diag(1, 3)                                      # prior covariance matrix of X[0]
+alpha1 = 20                                             # Prior for \sigma is Gamma (alpha1, beta1)
+alpha2 = 56                                             # Prior for \rho is Gamma (alpha2, beta2)
+alpha3 = 6                                              # Prior for \beta is Gamma (alpha3, beta3)
 beta1 = 0.5
 beta2 = 0.5
 beta3 = 0.5
 
-K = (tf - to) * Nobs
-N = (tf - to) / del_t
-R = diag(2,3)
+K = (tf - to) * Nobs                                    # no of real life observations, i.e. size of Y
+N = (tf - to) / del_t                                   # no of discretizations of the Lorenz-63, i.e. size of X
+R = diag(2,3)                                           # observational error
 
-X = euler_maruyama(rmvnorm(1, tau_o, lam_o), del_t, N, c(10,28, 8/3), diag(6,3))
-Y = X[, seq(1, N + 1, N / K)] + t(rmvnorm(K + 1, mean = rep(0, 3), sigma = R))
-init = runif(3 * N + 12, 0, 5)
-init[(3*N + 7):(3*N + 12)] = c(1,0,0,1,0,1)
+X = euler_maruyama(rmvnorm(1, tau_o, lam_o), del_t, N, c(10,28, 8/3), diag(6,3)) # generating sample from Lorenz-63
+Y = X[, seq(1, N + 1, N / K)] + t(rmvnorm(K + 1, mean = rep(0, 3), sigma = R))   # observations from Lorenz-63
+init = runif(3 * N + 12, 0, 5)                                                   # random initial values for MCMC
+init[(3*N + 7):(3*N + 12)] = c(1,0,0,1,0,1)                                      # inital \Sigma should also be positive semi definite
 
-chain = metrop(ludfun, init, nbatch = 1e3, scale = 0.2)
+chain = metrop(ludfun, init, nbatch = 1e3, scale = 0.2)                          # running MH
 
 #R = diag(2, nrow = 3)
 #inv_R = solve(R)
